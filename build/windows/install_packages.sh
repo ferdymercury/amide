1d16cd9dff78e873da87f6a6c2d55e9c183797fd
Gert Van Hoey <gert.vanhoey@gmail.com>
2023-05-17 16:43:52 +0200

    add script for installing packages

---
+++ build/windows/install_packages.sh
@@ -0,0 +1,18 @@
+pacman -S --noconfirm \
+    automake \
+    git \
+    gnome-doc-utils \
+    intltool \
+    mingw-w64-x86_64-autotools \
+    mingw-w64-x86_64-dcmtk \
+    mingw-w64-x86_64-ffmpeg \
+    mingw-w64-x86_64-gsl \
+    mingw-w64-x86_64-gtk-doc \
+    mingw-w64-x86_64-gtk2 \
+    mingw-w64-x86_64-gtk3 \
+    mingw-w64-x86_64-libgnomecanvas \
+    mingw-w64-x86_64-png2ico \
+    mingw-w64-x86_64-toolchain \
+    openssh \
+    vim \
+    unzip
